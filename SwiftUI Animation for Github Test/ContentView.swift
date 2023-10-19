7b0a4334aa7d58b115fbb3ad9a7eed8e3f54c116
Min Thu Khine <helloworldjustin9@gmail.com>
2023-10-18 23:05:54 -0400

    Initial Commit

---
+++ SwiftUI Animation for Github Test/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  SwiftUI Animation for Github Test
+//
+//  Created by Hoover on 10/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var people = ["Sheldon","Georgie","Missy","George","Mary","Cornie","Mandy"]
+    var body: some View {
+        
+        NavigationView {
+            List(people, id: \.self) { person in
+                Text(person)
+            }
+            .navigationTitle("Young Sheldon")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Animation for Github Test/SwiftUI_Animation_for_Github_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Animation_for_Github_TestApp.swift
+//  SwiftUI Animation for Github Test
+//
+//  Created by Hoover on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Animation_for_Github_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
